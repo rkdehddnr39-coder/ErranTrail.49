084167cc7add1a8f4de963c0f6f631d84df66694
oohgi <rkdehddnr4@gmail.com>
2025-11-28 20:00:07 +0900

    move update_2

--- Assets/05_Comp/Player.cs
+++ Assets/05_Comp/Player.cs
@@ -6,22 +6,35 @@ public class Player : MonoBehaviour
     float vAxis;
     public float speed;
     private Rigidbody rb;
+    public float rotSpeed;
     private Vector3 moveVec = Vector3.zero;
 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = this.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        transform.position += moveVec * speed * Time.deltaTime;
+
         if (moveVec != Vector3.zero)
         {
-
+            if(Mathf.Sign(transform.forward.x) != Mathf.Sign(moveVec.x) || Mathf.Sign(transform.forward.z) != Mathf.Sign(moveVec.z))
+            {
+                transform.Rotate(0,1,0);
+            }
+            transform.forward = Vector3.Lerp(transform.forward , moveVec , rotSpeed * Time.deltaTime);
         }
+
     }
 }
